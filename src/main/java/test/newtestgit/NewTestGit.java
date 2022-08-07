a055b5f855130b11fe6c65e9eff4e10fbdfa4140
buddhini <buddhinisam@hotmail.com>
2022-08-07 12:49:41 +0530

    Change again

--- src/main/java/test/newtestgit/NewTestGit.java
+++ src/main/java/test/newtestgit/NewTestGit.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
+ */ 
 
 package test.newtestgit;
 
@@ -12,6 +12,6 @@ package test.newtestgit;
 public class NewTestGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World me!");
     }
 }
